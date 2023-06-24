d3c773b60070fc009f65bc51e8a4bf2474d4a961
WowliamGuo <64669373+WowliamGuo@users.noreply.github.com>
2023-06-24 11:58:36 -0700

    spotify player

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
 
       <div className = "spotify-player">
                 
-                <iframe 
-                    src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4sWSpwq3LiO" 
-                    allowtransparency = "true" 
-                    allow = "encrypted-media" 
-                    title = "Peaceful Piano"
-                />
-
+          <iframe 
+              src="https://open.spotify.com/embed/playlist/37i9dQZF1DX4sWSpwq3LiO" 
+              allowtransparency = "true" 
+              allow = "encrypted-media" 
+              title = "Peaceful Piano"
+          />
+          
       </div> 
 
       <Tasks />
